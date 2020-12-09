9aeaee62c50e98520c495d389b16bacaa4f6a734
bijangaire97 <56812736+bijangaire97@users.noreply.github.com>
2020-12-08 20:14:42 -0600

    this is my second commit

--- src/main/java/mynewspaper/springmvc/config/AppConfig.java
+++ src/main/java/mynewspaper/springmvc/config/AppConfig.java
@@ -1,12 +1,22 @@
 package mynewspaper.springmvc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "mynewspaper.springmvc" })
 public class AppConfig {
+	@Bean
+	public ViewResolver configureViewResolver() {
+		InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
+		viewResolve.setPrefix("/WEB-INF/view/");
+		viewResolve.setSuffix(".jsp");
 
+		return viewResolve;
+	}
 }
